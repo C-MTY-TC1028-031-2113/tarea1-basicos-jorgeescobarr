ffd6c74e884ac0599a1f4ad5d4a77a01eeed733e
jorgeescobarr <a01285155@itesm.mx>
2021-08-23 16:37:16 -0500

    listo

--- assignments/02Peso/src/exercise.py
+++ assignments/02Peso/src/exercise.py
@@ -3,5 +3,5 @@ def peso():
     pf=float(input("dame el peso final: "))
     meses= float(input("dame la cantidad de meses: "))
     bpm= ((pi-pf)/meses)
-    print("lo que debes bajar por mes es: " + str(bpm))
+    print("lo que debes bajar por mes es:  " + str(bpm))
 peso()
