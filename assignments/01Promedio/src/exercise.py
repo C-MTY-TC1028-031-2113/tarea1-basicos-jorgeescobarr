6c79c789ce92f4a2f918dc0e3c9496367ebd9a68
jorgeescobarr <a01285155@itesm.mx>
2021-08-23 16:36:25 -0500

    listo

--- assignments/01Promedio/src/exercise.py
+++ assignments/01Promedio/src/exercise.py
@@ -4,6 +4,6 @@ def prom():
     calif3= float(input("Calificación materia: "))
     calif4= float(input("Calificación materia: "))
     prom= ((calif1+calif2+calif3+calif4)/4)
-    print("el promedio es: " + str(prom))
+    print("el promedio es:  " + str(prom))
 
 prom()
--- assignments/04PEMDAS/src/exercise.py
+++ assignments/04PEMDAS/src/exercise.py
@@ -1,10 +1,24 @@
 def main():
     from math import sqrt
     pass
+    a=4
+    b=5
+    import math
+    oper1= (2*(3/4)) + (4*(2/3)) - (3*(1/5)) + (5*(1/2))
+    print(oper1)
+    oper2= 2*(math.sqrt(35)**2)+4*(math.sqrt(36)**3)-6*(math.sqrt(49)**2)
+    print(oper2)
+    oper3= (a**3+2*b**2)/(4*a)
+    print(oper3)
+    oper4= (2*(a+b)**2 + (a*(a-b)**2))/(a*b)**2
+    print(oper4)
+    oper5= math.sqrt((a+b)**2 + 2**(a+b))/(2*a+2*b)**2
+    print(round(oper5,4))
+
     
    
 
 
 
 if __name__ == '__main__':
-    main()
+main()
